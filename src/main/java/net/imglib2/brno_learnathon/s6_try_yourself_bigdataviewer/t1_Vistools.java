0f30be1257e1f811ea6f08e26faaea37975b8019
Vladimír Ulman <ulman@fi.muni.cz>
2023-10-09 13:25:37 +0200

    s6/t1 more ideas on bdv vistools... break the t1 into several...

--- src/main/java/net/imglib2/brno_learnathon/s6_try_yourself_bigdataviewer/t1_Vistools.java
+++ src/main/java/net/imglib2/brno_learnathon/s6_try_yourself_bigdataviewer/t1_Vistools.java
@@ -3,12 +3,23 @@ package net.imglib2.brno_learnathon.s6_try_yourself_bigdataviewer;
 public class t1_Vistools {
 	//pull in stuff from FLorians DAIS 2017 repo...
 	//
+	//t1 - basics
 	//single channel,
 	//multip0le channels,
 	//setup view transforms,
-	//some BDVoptions magic?? at least is2D...
+
+	//t2 - options
+	//some BDVoptions magic??
+	//    at least is2D...
+	//    introduce time axis
+	//    show/hide scale bar... not sure about this one
+
+	//t3 - interaction with BDV/engine
+	//draw own spheres and lines (maybe t1)
 	//register listeners for anything...
 	//    paintera-like drawing into volume??? I don't know
 	//    open two BDV have one follow the first one and being always 90deg side view...??
+
+	//t4 - inside own window
 	//embed BDV into one's own window
 }
